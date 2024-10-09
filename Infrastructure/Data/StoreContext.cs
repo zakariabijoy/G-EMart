ce8b46dee6f26acc3ae167b9dfaa2dab16db2ed4
zakariabijoy <zakaria.bijoy@live.com>
2024-10-10 02:16:27 +0600

    API Basics : Setting up entity framework

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,9 @@
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class StoreContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Product> Products { get; set; }
+}
