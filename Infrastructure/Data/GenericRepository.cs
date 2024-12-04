3676b9e2e8e28e8b708dba18839aef1c086d0577
zakariabijoy <zakaria.bijoy@live.com>
2024-12-05 00:59:25 +0600

    The specification pattern: Implementing the generic repository methods

--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -1,6 +1,7 @@
 using System;
 using Core.Entities;
 using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data;
 
@@ -8,36 +9,37 @@ public class GenericRepository<T>(StoreContext context) : IGenericRepository<T> 
 {
     public void Add(T entity)
     {
-        throw new NotImplementedException();
+       context.Set<T>().Add(entity);
     }
 
     public bool Exists(int id)
     {
-        throw new NotImplementedException();
+        return context.Set<T>().Any(x => x.Id == id);
     }
 
-    public Task<T?> GetByIdAsync(int id)
+    public async Task<T?> GetByIdAsync(int id)
     {
-        throw new NotImplementedException();
+        return await context.Set<T>().FindAsync(id);
     }
 
-    public Task<IReadOnlyList<T>> ListAllAsync()
+    public async Task<IReadOnlyList<T>> ListAllAsync()
     {
-        throw new NotImplementedException();
+        return await context.Set<T>().ToListAsync();
     }
 
     public void Remove(T entity)
     {
-        throw new NotImplementedException();
+        context.Set<T>().Remove(entity);
     }
 
-    public Task<bool> SaveAllAsync()
+    public async Task<bool> SaveAllAsync()
     {
-        throw new NotImplementedException();
+        return await context.SaveChangesAsync() > 0;
     }
 
     public void Update(T entity)
     {
-        throw new NotImplementedException();
+        context.Set<T>().Attach(entity);
+        context.Entry(entity).State = EntityState.Modified; 
     }
 }
