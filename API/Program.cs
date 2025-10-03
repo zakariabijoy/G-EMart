db197c7627845da06a80d4c28e1896f02b034eaa
zakariabijoy <zakaria.bijoy@live.com>
2025-10-04 01:50:15 +0600

    Error handling on the API: Adding CORS support on the API

--- API/Program.cs
+++ API/Program.cs
@@ -17,12 +17,18 @@ builder.Services.AddDbContext<StoreContext>(opt => {
 });
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));    
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseCors(x => x.AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.UseSwagger();
 app.UseSwaggerUI();
