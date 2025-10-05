7b8b715a11fa5de6a2142219ef676525a13fa11b
zakariabijoy <zakaria.bijoy@live.com>
2025-10-06 03:44:27 +0600

    Angular Basics: Using Types in our Project

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -2,6 +2,8 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./layout/header/header";
 import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -13,10 +15,10 @@ export class App implements OnInit{
   baseurl = 'https://localhost:5001/api/';
   private http = inject(HttpClient);
   protected readonly title = signal('client');
-  products: any[] = [];
+  products: Product[] = [];
 
   ngOnInit(): void {
-    this.http.get<any>(this.baseurl + 'products').subscribe({
+    this.http.get<Pagination<Product>>(this.baseurl + 'products').subscribe({
       next: response =>  this.products = response.data,
       error: error =>  console.error(error),
       complete: () => console.log('Completed')
---
+++ client/src/app/shared/models/product.ts
@@ -0,0 +1,10 @@
+export type Product = {
+    id: number;
+    name: string;
+    descriptiion: string;
+    price: number;
+    pictureUrl: string;
+    type: string;
+    brand: string;
+    quantityInStock: number;
+}                                   
