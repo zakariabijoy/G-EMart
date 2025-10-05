35b7c9760a3879a4d15a66dc4e4c443bb9d5bf0b
zakariabijoy <zakaria.bijoy@live.com>
2025-10-06 01:46:28 +0600

    Angular Basics: Adding a Header component

--- client/src/app/layout/header/header.ts
+++ client/src/app/layout/header/header.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { MatButton } from '@angular/material/button';
+import { MatBadge } from '@angular/material/badge';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    MatIcon,
+    MatButton,
+    MatBadge,
+  ],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
