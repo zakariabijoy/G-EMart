7d9372c80a30a26a9dad5f85be15336b184c8860
zakariabijoy <zakaria.bijoy@live.com>
2025-10-06 01:27:31 +0600

    Angular Basics: Setting up the folder structure and creating components

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./layout/header/header";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
