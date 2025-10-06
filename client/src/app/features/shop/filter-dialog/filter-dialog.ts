1be4e5d165d5aae4e7055145715016233a04a2af
zakariabijoy <zakaria.bijoy@live.com>
2025-10-07 04:10:55 +0600

    Building the User Interface for the shop: Adding the filtering functionality using Material Dialog part 2

--- client/src/app/features/shop/filter-dialog/filter-dialog.ts
+++ client/src/app/features/shop/filter-dialog/filter-dialog.ts
@@ -1,9 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Shop } from '../../../core/services/shop';
 import { MatDivider } from '@angular/material/divider';
 import { MatSelectionList } from '@angular/material/list';
 import { MatListOption } from '@angular/material/list';
 import { MatButton } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-filter-dialog',
@@ -18,4 +19,16 @@ import { MatButton } from '@angular/material/button';
 })
 export class FilterDialog {
   protected shopService = inject(Shop);
+  private dialogRef = inject(MatDialogRef<FilterDialog>);
+  data = inject(MAT_DIALOG_DATA)
+
+  selectedBrands = signal<string[]>(this.data.selectedBrands);
+  selectedTypes = signal<string[]>(this.data.selectedTypes);
+
+  applyFilters() {
+    this.dialogRef.close({
+      selectedBrands: this.selectedBrands(),
+      selectedTypes: this.selectedTypes()
+    });
+  }
 }
--- client/src/app/features/shop/shop.ts
+++ client/src/app/features/shop/shop.ts
@@ -2,18 +2,28 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { Product } from '../../shared/models/product';
 import { Shop as ShopService}  from '../../core/services/shop';
 import { ProductItem } from './product-item/product-item';
+import { MatDialog } from '@angular/material/dialog';
+import { FilterDialog } from './filter-dialog/filter-dialog';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-shop',
   imports: [
-    ProductItem
+    ProductItem,
+    MatButton,
+    MatIcon
   ],
   templateUrl: './shop.html',
   styleUrl: './shop.scss'
 })
 export class Shop implements OnInit {
-  products = signal<Product[]>([]);
   private shopService = inject(ShopService);
+  private dialogService = inject(MatDialog);
+
+  products = signal<Product[]>([]);
+  selectedBrands = signal<string[]>([]);
+  selectedTypes = signal<string[]>([]);
 
   ngOnInit(): void {
     this.initializeShop();
@@ -25,6 +35,28 @@ export class Shop implements OnInit {
     this.shopService.getProducts().subscribe({
       next: response =>  this.products.set(response.data),
       error: error =>  console.error(error)
+    });
+  }
+
+  openFilterDialog() {
+    const dialogRef = this.dialogService.open(FilterDialog, {
+      minHeight: '500px',
+      data: {
+        selectedBrands: this.selectedBrands(),
+        selectedTypes: this.selectedTypes()
+      }
+    });
+
+    dialogRef.afterClosed().subscribe({
+      next: result => {
+        if(result) {
+          console.log(result);
+          this.selectedBrands.set(result.selectedBrands);
+          this.selectedTypes.set(result.selectedTypes);
+          //this.applyFilters();
+        }
+      },
+      error: error => console.error(error)
     });
   }
 }
