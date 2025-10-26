c2129a24ed184bd22a8d5a9f95e4cf084d40a1b1
zakariabijoy <zakaria.bijoy@live.com>
2025-10-27 01:57:30 +0600

    Angular Routing: Creating components and routes

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component,signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./layout/header/header";
-import { Shop } from './features/shop/shop';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Shop],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ client/src/app/features/shop/product-details/product-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-details',
+  imports: [],
+  templateUrl: './product-details.html',
+  styleUrl: './product-details.scss'
+})
+export class ProductDetails {
+
+}
