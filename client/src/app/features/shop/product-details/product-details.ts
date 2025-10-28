00f55a125bae2ba69521fd6e0988ecf4d6b2dd62
zakariabijoy <zakaria.bijoy@live.com>
2025-10-29 02:09:46 +0600

    Angular Routing: Designing the product details page

--- client/src/app/features/shop/product-details/product-details.ts
+++ client/src/app/features/shop/product-details/product-details.ts
@@ -2,10 +2,24 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { Shop } from '../../../core/services/shop';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../../../shared/models/product';
+import { CurrencyPipe } from '@angular/common';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { MatDivider } from '@angular/material/divider';
 
 @Component({
   selector: 'app-product-details',
-  imports: [],
+  imports: [
+    CurrencyPipe,
+    MatButton,
+    MatIcon,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatDivider
+  ],
   templateUrl: './product-details.html',
   styleUrl: './product-details.scss'
 })
--- client/src/app/shared/models/product.ts
+++ client/src/app/shared/models/product.ts
@@ -1,7 +1,7 @@
 export type Product = {
     id: number;
     name: string;
-    descriptiion: string;
+    description: string;
     price: number;
     pictureUrl: string;
     type: string;
